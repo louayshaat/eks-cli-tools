2dafa9e6ce953abb782f0c9591f96eef68cc9525
Louay Shaat <shaaloua@amazon.com>
2021-03-01 11:08:59 +0400

    Update cli_tools.sh

--- cli_tools.sh
+++ cli_tools.sh
@@ -2,19 +2,18 @@ cd ~
 sudo yum groupinstall "Development Tools" -y
 sudo yum install jq -y
 pip install --upgrade pip
-pip install awscliv2
+pip install awscli --upgrade
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 eval $(/home/ec2-user/.linuxbrew/bin/brew shellenv)
+source  ~/.bash_profile
 
 brew tap weaveworks/tap
 brew install kubernetes-cli kubernetes-helm weaveworks/tap/eksctl
-brew install helm
 brew install eksctl
 brew install gettext
-curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+
 
 source  ~/.bash_profile
 aws eks update-kubeconfig --name 'EKS-Lab'
-
-
